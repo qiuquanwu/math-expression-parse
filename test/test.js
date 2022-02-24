b438692aa8948e38a775e3f4293b4240a5d93653
qiuquanwu <919403255@qq.com>
2022-02-24 16:01:11 +0800

    添加单元测试

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@
 var { parse } = require('../dist/mep.cjs.js');
 var chai = require('chai'),
   expect = chai.expect
-describe('MEP', function () {
-  describe('#Empty Object', function () {
+describe('MEP单元测试', function () {
+  describe('#空值测试', function () {
     it('should return empty string', function () {
       expect(parse({}, "")).to.equal('')
     });
@@ -15,22 +15,22 @@ describe('MEP', function () {
       }, "{name}")).to.equal('张三')
     });
   });
-  describe('#sum', function () {
+  describe('#数学求和sum', function () {
     it('should return 3', function () {
       expect(parse({}, "sum(1,2)")).to.equal(3)
     });
   });
-  describe('#sum-2', function () {
+  describe('#数学求和sum-2', function () {
     it('should return 22', function () {
       expect(parse({
-        "age":20
+        "age": 20
       }, "sum({age},2)")).to.equal(22)
     });
   });
-  describe('#条件', function () {
+  describe('#条件测试', function () {
     it('should return 及格', function () {
       expect(parse({
-        "score":80
+        "score": 80
       }, "condition({score}>60,'及格','不及格')")).to.equal("及格")
     });
   });
